ef612616c7fdcd2f9696ac95c26710892bc7f766
Eric Cavalcante <g4mesh.bigorna@gmail.com>
2023-03-23 19:30:46 -0300

    Adicionando funcionalidade para adição de fotos diretamente do computador

--- galeria/models.py
+++ galeria/models.py
@@ -14,7 +14,7 @@ class Fotografia(models.Model):
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
     publicada = models.BooleanField(default=False)
-    foto = models.CharField(max_length=150, null=False, blank=False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
     data_fotografia = models.DateField(default=datetime.now, blank=False)
 
     def __str__(self):
