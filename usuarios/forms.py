f2d4e6a1be59a6a83eb22a79f87ef343dbf8195c
Eric Cavalcante <g4mesh.bigorna@gmail.com>
2023-04-04 16:39:49 -0300

    estilizando a página de cadastro

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -25,3 +25,52 @@ class LoginForms(forms.Form):
         )
     )
 
+class CadastroForms(forms.Form):
+    nome_cadastro=forms.CharField(
+        label ='Nome de Cadastro',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+        attrs={
+        'class': 'form-control',
+        'placeholder': 'Ex : João Silva'
+        }
+        )
+    )
+
+    email=forms.EmailField(
+         label ='Email',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+        attrs={
+        'class': 'form-control',
+        'placeholder': 'Ex : joaosilva@email.com'
+        }
+        )
+    )
+
+    senha1=forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+        attrs={
+        'class': 'form-control',
+        'placeholder': 'Digite a sua senha'
+        }
+        )
+    )
+
+    senha2=forms.CharField(
+        label='Confirme a sua senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+        attrs={
+        'class': 'form-control',
+        'placeholder': 'Digite a sua senha novamente'
+        }
+        )
+    )
+
