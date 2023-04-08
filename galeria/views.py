ad6bfdeca231d1e62ed200c809fe72aa67c868bc
Eric Cavalcante <g4mesh.bigorna@gmail.com>
2023-04-07 22:15:25 -0300

    Criando o sistema de autenticação de usuarios

--- galeria/views.py
+++ galeria/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from galeria.models import Fotografia
+from django.contrib import messages
 
     # 2:{"nome": "Gláxia NGC 1079",
     #    "legenda": "nasa.org / NASA / Hubble"},
 
 def index(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Realize o login para acessar a página")
+        return redirect ('login')
+    
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
@@ -13,6 +18,10 @@ def imagem(request, foto_id):
     return render(request, 'galeria/imagem.html', {"fotografia" :fotografia})
 
 def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Realize o login para acessar a página")
+        return redirect ('login')
+    
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
 
     if "buscar" in request.GET:
